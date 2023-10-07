3309b820143378821769244bea4ed1a63641a150
JoonPark <sargadil@gmail.com>
2023-10-08 01:19:46 +0900

    Next13 App Router

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className=" p-24 flex min-h-screen flex-col items-center justify-between">
+      <div className="text-6xl">
+        안녕하세요
+      </div>
+    </main>
+  );
+}
