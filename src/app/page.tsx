8d61441cc3d11ada32c555a508f3e96b42e7e725
JoonPark <sargadil@gmail.com>
2023-10-08 11:40:48 +0900

    Tailwind + gitkeep

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className=" p-24 flex min-h-screen flex-col items-center justify-between">
-      <div className="text-6xl">
+      <div className="text-6xl z-modal">
         안녕하세요
       </div>
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -13,8 +13,14 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      zIndex: {
+        modal: '50',
+        nav: '40',
+        sticky: '20',
+        main: '0',
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
